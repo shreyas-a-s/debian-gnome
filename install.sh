04bd4b560bf0109785090929f9ddef6f042fcbdf
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-10 22:20:19 +0530

    Add choice of installing debian-customisation

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# Functions
+function choiceOfCustomisation {
+   read -r -p "Continue to install debian-customisation? (yes/no): " choice
+   case "$choice" in 
+     "yes" ) echo "Starting the installation.."; cd ..; git clone -b de https://github.com/shreyas-a-s/debian-customisation.git && cd debian-customisation/ && ./install.sh;;
+     "no" ) exit 1;;
+     * ) echo "Invalid Choice! Keep in mind this is CASE-SENSITIVE."; choiceOfCustomisation;;
+   esac
+}
+
 sudo apt install gnome-core -y
 sudo apt purge firefox-esr yelp -y && sudo apt install gnome-console gnome-tweaks -y && sudo apt autoremove -y
+
+# Install debian-customisation
+choiceOfCustomisation
