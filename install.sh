6b0d5dd9dac4f7ef6b261f6405d9b5500ff257ed
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-22 12:48:43 +0530

    CleanUp

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Functions
-function choiceOfCustomisation {
-   read -r -p "Continue to install debian-customisation? (yes/no): " choice
-   case "$choice" in 
-     "yes" ) echo "Starting the installation.."; cd ..; git clone -b de https://github.com/shreyas-a-s/debian-customisation.git && cd debian-customisation/ && ./install.sh;;
+# Customisation Choice
+function customisationChoice {
+   read -r -p "Continue to install debian-customisation? (yes/no): " customisation_choice
+   case "$customisation_choice" in 
+     "yes" ) echo "Starting the installation.."; (cd ..; git clone -b de https://github.com/shreyas-a-s/debian-customisation.git && cd debian-customisation/ && ./install.sh);;
      "no" ) exit 1;;
-     * ) echo "Invalid Choice! Keep in mind this is CASE-SENSITIVE."; choiceOfCustomisation;;
+     * ) echo "Invalid Choice! Keep in mind this is CASE-SENSITIVE."; customisationChoice;;
    esac
 }
 
+# Installation
 sudo apt install gnome-core -y
 sudo apt purge firefox-esr yelp -y && sudo apt install gnome-console gnome-tweaks -y && sudo apt autoremove -y
 
 # Install debian-customisation
-choiceOfCustomisation
+customisationChoice
