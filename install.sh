bbac85716823dc3cd2b650e6a6a24cfbd501826f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-22 14:22:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Customisation Choice
+# Customisation choice
 function customisationChoice {
-   read -r -p "Continue to install debian-customisation? (yes/no): " customisation_choice
-   case "$customisation_choice" in 
-     "yes" ) echo "Starting the installation.."; (cd ..; git clone -b de https://github.com/shreyas-a-s/debian-customisation.git && cd debian-customisation/ && ./install.sh);;
-     "no" ) exit 1;;
-     * ) echo "Invalid Choice! Keep in mind this is CASE-SENSITIVE."; customisationChoice;;
-   esac
+	read -r -p "Continue to install debian-customisation? (yes/no): " customisation_choice
+	if [ "$customisation_choice" != 'yes' ] && [ "$customisation_choice" != 'no' ]; then
+		echo -e "Invalid Choice! Keep in mind this is CASE-SENSITIVE.\n"
+    	customisationChoice
+  	fi
 }
 
 # Installation
+customisationChoice && export customisation_choice
 sudo apt install gnome-core -y
 sudo apt purge firefox-esr yelp -y && sudo apt install gnome-console gnome-tweaks -y && sudo apt autoremove -y
 
-# Install debian-customisation
-customisationChoice
+# Installing customisations
+if [ "$customisation_choice" = 'yes' ]; then
+  echo "Starting the installation.."
+  cd ..
+  git clone -b de https://github.com/shreyas-a-s/debian-customisation.git && cd debian-customisation/ && ./install.sh
+fi
