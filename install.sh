2ba105b8c3f6295cc922d86905b97098cad7eff8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-22 14:43:52 +0530

    Some high level stuff

--- install.sh
+++ install.sh
@@ -9,8 +9,30 @@ function customisationChoice {
   	fi
 }
 
+# QEMU Choice
+function qemuChoice {
+  read -r -p "Continue to install qemu and virt-manager? (yes/no): " qemu_choice
+  if [ "$qemu_choice" != 'yes' ] && [ "$qemu_choice" != 'no' ]; then
+    echo -e "Invalid Choice! Keep in mind this is CASE-SENSITIVE.\n"
+    qemuChoice
+  fi
+}
+
+# Shell Choice
+function shellChoice {
+	read -r -p "Which shell you prefer? (bash/fish) : " shell_choice
+  if [ "$shell_choice" != 'bash' ] && [ "$qemu_choice" != 'fish' ]; then
+    echo -e "Invalid Choice! Keep in mind this is CASE-SENSITIVE.\n"
+    shellChoice
+  fi
+}
+
 # Installation
 customisationChoice && export customisation_choice
+if [ "$customisation_choice" = 'yes' ]; then
+  qemuChoice
+  shellChoice && export shell_choice
+fi
 sudo apt install gnome-core -y
 sudo apt purge firefox-esr yelp -y && sudo apt install gnome-console gnome-tweaks -y && sudo apt autoremove -y
 
