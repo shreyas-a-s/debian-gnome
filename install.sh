9df8aa0100b88589fed5b603e09183a9c7f5cb98
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-09 23:05:18 +0530

    Remove byobu & epiphany-browser

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ if [ "$customisation_choice" = 'yes' ]; then
   shellChoice && export shell_choice
 fi
 sudo apt-get -y install gnome-core
-sudo apt-get -y purge firefox-esr yelp gnome-terminal totem gnome-software gnome-characters gnome-contacts gnome-font-viewer gnome-logs && sudo apt-get -y install gnome-console gnome-tweaks gnome-text-editor gnome-shell-extension-gsconnect && sudo apt-get -y autoremove
+sudo apt-get -y purge firefox-esr yelp gnome-terminal totem gnome-software gnome-characters gnome-contacts gnome-font-viewer gnome-logs byobu epiphany-browser && sudo apt-get -y install gnome-console gnome-tweaks gnome-text-editor gnome-shell-extension-gsconnect && sudo apt-get -y autoremove
 
 # Symlink gedit to gnome-text-editor
 sudo ln -s /usr/bin/gnome-text-editor /usr/bin/gedit
